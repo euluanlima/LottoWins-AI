edb06056a3acd1e4f51446e6e3338852928977dd
euluanlima <luanmatos1n@gmail.com>
2025-05-01 00:17:13 -0300

    lottowins v23

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  trailingSlash: false, // Explicitly set trailingSlash to false
+  output: "export", // Add output: "export" based on successful build log
+  trailingSlash: false, // Keep trailingSlash: false as it might still be needed
 };
 
 export default nextConfig;
