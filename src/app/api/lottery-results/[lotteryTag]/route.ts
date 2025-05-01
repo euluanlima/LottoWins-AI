7b270ebf855113b06452aa24a5acfa342df47640
euluanlima <luanmatos1n@gmail.com>
2025-04-30 22:55:16 -0300

    lottowins v13

--- src/app/api/lottery-results/[lotteryTag]/route.ts
+++ src/app/api/lottery-results/[lotteryTag]/route.ts
@@ -11,7 +11,7 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(
   request: Request,
-  { params }: { params: { lotteryTag: string } }
+  { params } // Let TypeScript infer the type for params
 ) {
   const lotteryTag = params.lotteryTag;
 
